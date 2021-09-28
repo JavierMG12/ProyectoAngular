93f79ce2f0fca89c6f404dca72a19f943969ea5b
Javier Mendoza <javiermendozaglz@icloud.com>
2021-09-27 21:59:26 -0500

    Modificaciones recientes

---
+++ src/app/Components/list-comentario/list-comentario.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Comentario } from 'src/app/Interface/comentario';
+import { BrowserModule } from '@angular/platform-browser'
+
+
+@Component({
+  selector: 'app-list-comentario',
+  templateUrl: './list-comentario.component.html',
+  styleUrls: ['./list-comentario.component.css']
+})
+export class ListComentarioComponent implements OnInit {
+
+  listcomentario: Comentario[] = [
+    {titulo:'Practica',creador:'Javier', texto:'Complemento',fechacreacion: new Date()},
+    {titulo:'Practica',creador:'Javier', texto:'Complemento',fechacreacion: new Date()}
+  ]
+
+  
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { AgregarEditarComentarioComponent } from './Components/agregar-editar-comentario/agregar-editar-comentario.component';
+import { ListComentarioComponent } from './Components/list-comentario/list-comentario.component';
+import { VerComentarioComponent } from './Components/ver-comentario/ver-comentario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AgregarEditarComentarioComponent,
+    ListComentarioComponent,
+    VerComentarioComponent
   ],
   imports: [
     BrowserModule
